89c1d8a16944434665aa4a6f9370a05c24dd1b3a
Zachary Silver <zmansilver@gmail.com>
2021-08-15 13:01:31 -0700

    updated foreground color again

--- config.def.h
+++ config.def.h
@@ -120,7 +120,7 @@ static const char *colorname[] = {
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#232731",	/*	256: background	*/
-	"#c2c1b8",	/*	257: foreground	*/
+	"#bdbdbd",	/*	257: foreground	*/
 };
 
 
